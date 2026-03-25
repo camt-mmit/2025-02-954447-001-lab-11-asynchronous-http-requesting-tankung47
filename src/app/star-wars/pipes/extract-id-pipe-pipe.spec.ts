93203c3782ae2a726e5a30e83016164d40ba3fe4
Pongsapak Promwungsri <tankung47@gmail.com>
2026-03-26 00:07:39 +0700

    Auto-generated commit

--- src/app/star-wars/pipes/extract-id-pipe-pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ExtractIdPipe } from './extract-id-pipe-pipe';
-
-describe('ExtractIdPipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new ExtractIdPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
